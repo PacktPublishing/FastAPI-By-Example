a03949e7823c84d2bab6b634e5c38dae004369a9
ehigie aito (Pystar) <aitoehigie@gmail.com>
2021-04-25 01:59:07 +0100

    adds a Dependency Injection code sample

--- ch2/chapter2_features_of_fastapi_01.py
+++ ch2/chapter2_features_of_fastapi_01.py
@@ -1,3 +1,16 @@
-def message(age:int = 0, name:str = 'stranger') -> str:
-    return f'Hello {name}, you are {age} years old'
+import uvicorn
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
+
+def get_db():
+    db = 'fakedatabaseconnection'
+    return db
+
+app = FastAPI()
+
+@app.get('/')
+async def index(db:Session=Depends(get_db)):
+    return f'Fake database connection: {db}'
+
+uvicorn.run(app, host='127.0.0.1', port=8080)
---
+++ ch2/chapter2_features_of_fastapi_02.py
@@ -0,0 +1,3 @@
+def message(age:int = 0, name:str = 'stranger') -> str:
+    return f'Hello {name}, you are {age} years old'
+
